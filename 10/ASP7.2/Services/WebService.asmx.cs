42b6d52573ba10db59871c9ca5b2190dcab78686
klepaski <julia.klepaski@gmail.com>
2020-07-05 10:33:17 +0300

    ekz

---
+++ 10/ASP7.2/Services/WebService.asmx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using Newtonsoft.Json;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ASP7._2.Services
+{
+    [WebService(Namespace = "http://julia.org/")]                 //пр-во имен (smw60.by)
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]   //спецификация совместимости веб-служб
+    [System.ComponentModel.ToolboxItem(false)]                      //управление панелью эл-тов VS ToolBox
+
+
+    public class WebService : System.Web.Services.WebService
+    {
+        private int x = 0;
+
+        [WebMethod(Description = "Установить значение x", EnableSession = true)]
+        public void SetX(int x)
+        {
+            //this.x = x;
+            this.Session["x"] = x;
+        }
+
+        [WebMethod(Description = "Получить значение x", EnableSession = true)]
+        public int GetX()
+        {
+            //return this.x;
+            int rc = 777;
+            if (this.Session["x"] != null) rc = (int)this.Session["x"];
+            return rc;
+        }
+
+        [WebMethod(Description = "Увеличить На x и вернуть", EnableSession = true)]
+        public int AddX(int x)
+        {
+            //return this.x += x;
+            int rc = 777;
+            if (this.Session["x"] != null) rc = (int)this.Session["x"] + x;
+            return rc;
+        }
+    }
+}
+
+//http://localhost:49828/Services/WebService.asmx?wsdl
---
+++ 19/WFWebService/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace WFWebService
+{
+    public partial class Form1 : Form
+    {
+        WS.Service1Client client;
+
+        private void makeColumnOrder()
+        {
+            dataGridView1.Columns["Id"].DisplayIndex = 0;
+            dataGridView1.Columns["FIO"].DisplayIndex = 1;
+            dataGridView1.Columns["Telephone"].DisplayIndex = 2;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.client = new WS.Service1Client();
+            UpdateTable();
+            makeColumnOrder();
+        }
+
+        public void UpdateTable()
+        {
+            List<WS.Phone> phones = this.client.GetDict().ToList();
+            WS.Phone phone = new WS.Phone();
+            phone.Id = 0;
+            phone.FIO = "";
+            phone.Telephone = "";
+            phones.Add(phone);
+            dataGridView1.DataSource = phones;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+			UpdateTable();
+        }
+    }
+}
